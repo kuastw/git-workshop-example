7d94cc2aeb0e13b64898f2c4cdb9456385f605f6
aweimeow <ccps960718456@gmail.com>
2016-07-03 11:22:09 +0800

    A push 1

--- kuas.py
+++ kuas.py
@@ -61,3 +61,25 @@ if __name__ == '__main__':
 	else:
 		print( 'Please input a valid number !' )
 """
+
+
+# Here for person A
+
+import requests
+from lxml import etree
+
+def getlist():
+	url = 'http://academic.kuas.edu.tw/bin/home.php'
+	News = list()
+	s = requests.session()
+	r = s.get( url )
+	
+	# Set The response's encoding
+	r.encoding = 'utf-8'
+	
+	tree = etree.HTML( r.text )
+	bulletin_table = tree.xpath( '//table[@summary="list"]//a[@title]' )
+	for t in bulletin_table:
+		News.append( [ t.text, t.attrib[ 'href' ] ] )
+
+	return News
